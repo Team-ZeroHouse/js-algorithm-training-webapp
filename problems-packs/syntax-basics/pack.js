a09f42788f9f42c157583fdebf071b2b264e34f0
T.SooranKim <T.SooranKim@gmail.com>
2020-09-13 18:43:45 +0900

    levels 내부 level 객체에 level property를 id로 변경

--- problems-packs/syntax-basics/pack.js
+++ problems-packs/syntax-basics/pack.js
@@ -5,8 +5,8 @@
     title: '기본 문법 훈련',
     levels: [
       {
+        id: 'operators',
         title: '기본 연산자',
-        level: 'operators',
         descripiton: '기본적인 입력과 출력을 훈련합니다.',
         problems: [
           {

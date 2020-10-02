0c0efcc62c8bad3841922ac6e3791aa62b102e61
T.SooranKim <t.soorankim@gmail.com>
2020-10-02 22:01:00 +0900

    index.js 코드 감싸기

--- js/index.js
+++ js/index.js
@@ -1,56 +1,62 @@
-const packs = {};
+window.loadedProblemPakcs = undefined;
+window.loadedProblemPack = undefined;
 
-function loadedProblemPakcs(packs)
+(function()
 {
-  packs.forEach(pack =>
+  const packs = {};
+
+  window.loadedProblemPakcs = function(packs)
   {
-    packs[pack.id] = null;
-    const s = document.createElement('script');
-    s.src = `problems-packs/${pack.id}/pack.js`;
-    document.body.appendChild(s);
-
-    const $li = $('<li />');
-    const $a = $('<a />');
-    $a.attr('href', `#${pack.id}`);
-    $a.text(pack.title);
-    $li.append($a);
-    $('#packs').append($li);
-  });
-}
-
-function loadedProblemPack(pack)
-{
-  packs[pack.id] = pack;
-  changePack();
-}
-
-function changePack()
-{
-  const packId = location.hash.replace(/^#/, '');
-  if (packId.length === 0)
-  {
-    $('#levels').html('');
-    return;
-  }
-
-  if (packId in packs)
-  {
-    $('#levels').html('');
-    
-    packs[packId].levels.forEach(function(level)
+    packs.forEach(pack =>
     {
-      const $levelLi = $(`<li><h3>${level.title}</h3><p>${level.descripiton}</p></li>`);
-      const $levels = $('<ol>');
-      $levelLi.append($levels);
-      level.problems.forEach(function(problem)
-      {
-        const $problem = $(`<li><a href="solve.html?pack=${packId}&level=${level.id}&problem=${problem.id}">${problem.name}</a></li>`);
-        $levels.append($problem);
-      });
-      
-      $('#levels').append($levelLi);
+      packs[pack.id] = null;
+      const s = document.createElement('script');
+      s.src = `problems-packs/${pack.id}/pack.js`;
+      document.body.appendChild(s);
+
+      const $li = $('<li />');
+      const $a = $('<a />');
+      $a.attr('href', `#${pack.id}`);
+      $a.text(pack.title);
+      $li.append($a);
+      $('#packs').append($li);
     });
   }
-}
 
-$(window).on('hashchange', changePack);
+  window.loadedProblemPack = function(pack)
+  {
+    packs[pack.id] = pack;
+    changePack();
+  }
+
+  function changePack()
+  {
+    const packId = location.hash.replace(/^#/, '');
+    if (packId.length === 0)
+    {
+      $('#levels').html('');
+      return;
+    }
+
+    if (packId in packs)
+    {
+      $('#levels').html('');
+      
+      packs[packId].levels.forEach(function(level)
+      {
+        const $levelLi = $(`<li><h3>${level.title}</h3><p>${level.descripiton}</p></li>`);
+        const $levels = $('<ol>');
+        $levelLi.append($levels);
+        level.problems.forEach(function(problem)
+        {
+          const $problem = $(`<li><a href="solve.html?pack=${packId}&level=${level.id}&problem=${problem.id}">${problem.name}</a></li>`);
+          $levels.append($problem);
+        });
+        
+        $('#levels').append($levelLi);
+      });
+    }
+  }
+
+  $(window).on('hashchange', changePack);
+})();

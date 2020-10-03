bf35a4c044927af625ef0ea3eb83ee696db5cd9b
T.SooranKim <t.soorankim@gmail.com>
2020-10-03 16:49:10 +0900

    문제 리스트 마크업 변경

--- js/index.js
+++ js/index.js
@@ -3,60 +3,54 @@ window.loadedProblemPack = undefined;
 
 (function()
 {
-  const packs = {};
-
   window.loadedProblemPakcs = function(packs)
   {
     packs.forEach(pack =>
     {
-      packs[pack.id] = null;
       const s = document.createElement('script');
       s.src = `problems-packs/${pack.id}/pack.js`;
       document.body.appendChild(s);
 
-      const $li = $('<li />');
-      const $a = $('<a />');
-      $a.attr('href', `#${pack.id}`);
-      $a.text(pack.title);
-      $li.append($a);
-      $('#packs').append($li);
+      const $li = $(`
+        <li data-pack-id="${pack.id}">
+          <header>
+            <a href="#${pack.id}">${pack.title}</a>
+          </header>
+          <ul class="levels"></li>
+        </li>
+      `);
+      $('#problems-packs').append($li);
     });
   }
 
   window.loadedProblemPack = function(pack)
   {
-    packs[pack.id] = pack;
-    changePack();
-  }
-
-  function changePack()
-  {
-    const packId = location.hash.replace(/^#/, '');
-    if (packId.length === 0)
+    const $levels = $(`li[data-pack-id=${pack.id}] ul`);
+    for (const level of pack.levels)
     {
-      $('#levels').html('');
-      return;
-    }
+      const $li = $(`
+        <li data-level-id="${level.id}">
+          <header>
+            <h3>${level.title}</h3>
+            <p>${level.description}</p>
+          </header>
 
-    if (packId in packs)
-    {
-      $('#levels').html('');
-      
-      packs[packId].levels.forEach(function(level)
+          <ol class="problems"></ol>
+        </li>
+      `);
+      $levels.append($li);
+
+      const $problems = $li.find('ol.problems');
+      for (const problem of level.problems)
       {
-        const $levelLi = $(`<li><h3>${level.title}</h3><p>${level.descripiton}</p></li>`);
-        const $levels = $('<ol>');
-        $levelLi.append($levels);
-        level.problems.forEach(function(problem)
-        {
-          const $problem = $(`<li><a href="solve.html?pack=${packId}&level=${level.id}&problem=${problem.id}">${problem.name}</a></li>`);
-          $levels.append($problem);
-        });
-        
-        $('#levels').append($levelLi);
-      });
+        const link = `solve.html?pack=${pack.id}&level=${level.id}&problem=${problem.id}`;
+        const $li = $(`
+          <li>
+            <a href="${link}">${problem.name}</a>
+          </li>
+        `);
+        $problems.append($li);
+      }
     }
   }
-
-  $(window).on('hashchange', changePack);
 })();

d83f5545a15404ac6a4d56390f4db3787c89b030
T.SooranKim <t.soorankim@gmail.com>
2021-06-03 17:28:12 +0900

    넓이를 높이로 변경

--- problems-packs/syntax-basics/operators/rect_area.js
+++ problems-packs/syntax-basics/operators/rect_area.js
@@ -1,5 +1,5 @@
 (function()
 {
-  const problem = {"title":"직사각형 넓이 구하기","content":"직사각형의 넓이는 밑변과 높이의 곱이다.\n직사각형의 넓이를 출력하라.\n\n### 입력\n밑변과 넓이가 공백을 기준으로 한줄에 출력된다.\n\n### 출력\n직사각형의 넓이를 출력하라.","openTestCases":[{"input":"5 2","output":"10"},{"input":"3 4","output":"12"},{"input":"10 24","output":"240"}],"randomTestCase":"const l = Math.floor(Math.random() * 1000);\r\nconst h = Math.floor(Math.random() * 1000);\r\nprintInput([l, h].join(' '))\r\nprintOutput(l*h);","solution":"const splited = readline().split(' ');\r\nconst l = parseInt(splited[0]);\r\nconst h = parseInt(splited[1]);\r\nprint(l*h);"}
+  const problem = {"title":"직사각형 넓이 구하기","content":"직사각형의 넓이는 밑변과 높이의 곱이다.\n직사각형의 넓이를 출력하라.\n\n### 입력\n밑변과 높이가 공백을 기준으로 한줄에 출력된다.\n\n### 출력\n직사각형의 넓이를 출력하라.","openTestCases":[{"input":"5 2","output":"10"},{"input":"3 4","output":"12"},{"input":"10 24","output":"240"}],"randomTestCase":"const l = Math.floor(Math.random() * 1000);\r\nconst h = Math.floor(Math.random() * 1000);\r\nprintInput([l, h].join(' '))\r\nprintOutput(l*h);","solution":"const splited = readline().split(' ');\r\nconst l = parseInt(splited[0]);\r\nconst h = parseInt(splited[1]);\r\nprint(l*h);"}
   loadedProblem(problem);
 })();
